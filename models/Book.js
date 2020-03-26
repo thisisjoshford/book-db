29d0f6ccf9c2ef5d6ce97d3204716b5146a1f624
Josh Ford <thisisjoshford@gmail.com>
2020-03-26 06:59:00 -0700

    book schema

---
+++ models/Book.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const bookSchema = new mongoose.Schema({
+  author: {
+    type: String,
+    required: true
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  genre: {
+    type: String,
+    required: true,
+    enum: ['fiction', 'non-fiction']
+  }
+});
+
+module.exports = mongoose.model('book', bookSchema);
