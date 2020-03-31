75f4a9588afa626bf60df0891cf23e4680d1c899
Josh Ford <thisisjoshford@gmail.com>
2020-03-31 16:48:19 -0700

    notation removal

--- db/seed.js
+++ db/seed.js
@@ -2,15 +2,8 @@ const Book = require('../models/Book');
 const Library = require('../models/Library');
 const chance = require('chance').Chance();
 
-// specifying the number of tweets to create with our seed function
 module.exports = async({ booksToCreate = 100, librarysToCreate = 10 } = {}) => {
-  // creating tweets
-  // creating an array of tweetsToCreate length
-  // map through the array
-  // -> for each item in the array we create an object with { handle, text }
-  // for each tweet in the mapped array we create a tweet in our mongodb
   const genre = ['fiction', 'non-fiction'];
-
 
   const library = await Library.create([...Array(booksToCreate)].map(() => ({
     name: `${chance.name()}'s Library`,
