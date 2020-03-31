0a224833ce6367f59243d0f8a8f8bdd192d6f19b
Josh Ford <thisisjoshford@gmail.com>
2020-03-31 16:02:33 -0700

    location model

---
+++ models/Location.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const librarySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  address: {
+    type: String,
+    required: true
+  },
+  city: {
+    type: String,
+    required: true
+  },
+  state:{
+    type: String,
+    required: true
+  }
+});
+
+module.exports = mongoose.model('Library', librarySchema);
